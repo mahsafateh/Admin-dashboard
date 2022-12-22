b3816ff76d42f8207b1e96570b59d56e2e128c4d
Mahsa Fateh <mahsafaateh@gmail.com>
2022-12-22 23:35:57 +0300

    Project Finished

---
+++ assets/scripts/order.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName :'Flodable Mini Dorne',
+    productNumber : '65639',
+    paymentStatus : 'Due',
+    shipping : 'Pending',
+  },
+  {
+    productName :'LARVENDER KF106 Dorne',
+    productNumber : '64951',
+    paymentStatus : 'Due',
+    shipping : 'Declined',
+  },
+  {
+    productName :'Ruko F11 Pro Dorne',
+    productNumber : '65639',
+    paymentStatus : 'Due',
+    shipping : 'Pending',
+  },
+  {
+    productName :'Dorne with Camera Dorne',
+    productNumber : '65639',
+    paymentStatus : 'Due',
+    shipping : 'Deliverd',
+  },
+  {
+    productName :'GPS 4K Dorne',
+    productNumber : '65639',
+    paymentStatus : 'Due',
+    shipping : 'Deliverd',
+  },
+  {
+    productName :'DJI Air 26',
+    productNumber : '65639',
+    paymentStatus : 'Due',
+    shipping : 'Pending',
+  },
+  {
+    productName :'Lozenge Dorne',
+    productNumber : '65639',
+    paymentStatus : 'Paid',
+    shipping : 'Deliverd',
+  },
+]
